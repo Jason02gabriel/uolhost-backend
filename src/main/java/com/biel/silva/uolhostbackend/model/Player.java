daaeda50eb0f598edd053becde258e961e01e528
Jason_Gabriel <j.gabrielaraujodasilva@gmail.com>
2024-05-14 09:42:24 -0300

    feat: Player Entity created

---
+++ src/main/java/com/biel/silva/uolhostbackend/model/Player.java
@@ -0,0 +1,27 @@
+package com.biel.silva.uolhostbackend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "players")
+@Table(name = "players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email;
+    private String phoneNumber;
+    private String codename;
+    private GroupType groupType;
+}
